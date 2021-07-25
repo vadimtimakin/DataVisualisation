0b4fd3027760361065bf51568a5e1b6832b8d22c
Vadim Timakin <vdm.timakin@yandex.ru>
2021-07-25 08:18:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 
-df = pd.DataFrame()
-length = 100
+df = pd.read_csv("/tmp/data/test.csv")
+length = len(df)
 df["age"] = [34 for _ in range(length)]
 df.to_csv("/var/log/result.csv", index=False)
